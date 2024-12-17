f8f5ef3d4141c361714320bad1b93af400ad7a04
Alessandro Belotti <alessandro.belotti2004@gmail.com>
2024-12-17 10:09:12 +0100

    Google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import { Toaster } from "@src/components/ui/sonner";
 import "./globals.css";
 import { poppins } from "@src/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  other: {
+    "google-site-verification": "gsFkI_lyTlImqXu_3cBF6px8zgIEUL_HlzzVZnQxxeg",
+  },
+};
 
 export default function RootLayout({
   children,
