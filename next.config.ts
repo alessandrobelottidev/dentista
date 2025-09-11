10f7d382c06e5fbd5c5f7a66f2d64ae99637893f
Alessandro Belotti <alessandro.belotti2004@gmail.com>
2025-09-11 09:07:59 +0200

    Add image configuration to disable Netlify's optimized images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Using default output mode for Netlify with server components
+  images: {
+    unoptimized: true // let's not use netlify's optimized images (they use edge functions)
+  }
 };
 
 export default nextConfig;
